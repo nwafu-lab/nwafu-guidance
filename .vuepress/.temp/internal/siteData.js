dc1de1c2a023af010e42e350be5e4e5661d4dfab
神奇のDz <miku@ecy.pink>
2023-06-26 18:03:44 +0800

    修正.gitignore，Workflows添加目标仓库选项

--- .vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/nwafu-guidance/\",\"lang\":\"zh-CN\",\"title\":\"西农新手村\",\"description\":\"西北农林科技大学新手指南，使用指南，此指南针对新生编写，为新生入学提供帮助\",\"head\":[],\"locales\":{}}")
