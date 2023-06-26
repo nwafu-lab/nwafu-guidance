b94db215800f6a190917c2f2fa770b59d51579e5
神奇のDz <miku@ecy.pink>
2023-06-26 16:46:59 +0800

    Update

--- .vuepress/.temp/pages/index.html.js
+++ .vuepress/.temp/pages/index.html.js
@@ -1,1 +1,14 @@
 export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"首页\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"heroImage\":\"https://pic.mikupan.com/vCxPSU.png\",\"heroAlt\":\"西北农林科技大学校徽\",\"tagline\":\"此指南针对新生编写，为新生入学提供帮助\",\"title\":\"首页\",\"actions\":[{\"text\":\"开始\",\"link\":\"/\",\"type\":\"primary\"},{\"text\":\"Github\",\"link\":\"https://github.com/nwafu-lab/nwafu-guidance\",\"type\":\"secondary\"}]},\"headers\":[],\"git\":{\"updatedTime\":1687765857000,\"contributors\":[{\"name\":\"神奇のDz\",\"email\":\"miku@ecy.pink\",\"commits\":1}]},\"filePathRelative\":\"index.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,7 +4,7 @@ export default  defineUserConfig({
     lang: 'zh-CN',
     title: '西农新手村',
     description: '西北农林科技大学新手指南，使用指南，此指南针对新生编写，为新生入学提供帮助',
-    base: '/',
+    base: '/nwafu-guidance',
     head: [],
     theme: defaultTheme({
         navbar: [
