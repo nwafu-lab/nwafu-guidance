0900240354e332d178fa06f101c604d9096c865a
神奇のDz <miku@ecy.pink>
2023-06-26 17:29:59 +0800

    修正文件路径

--- .vuepress/.temp/internal/siteData.js
+++ .vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
 export const siteData = JSON.parse("{\"base\":\"/nwafu-guidance/\",\"lang\":\"zh-CN\",\"title\":\"西农新手村\",\"description\":\"西北农林科技大学新手指南，使用指南，此指南针对新生编写，为新生入学提供帮助\",\"head\":[],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
